9fba6ba484afe5ca6c80b72261eb22f2811dd946
rab.mihaly.jozsef <rabmisi1010@gmail.com>
2025-05-20 13:00:54 +0200

    css ver1.0 added

--- frontend/src/components/login/Form.jsx
+++ frontend/src/components/login/Form.jsx
@@ -4,20 +4,20 @@ import {Link} from 'react-router-dom';
 function Form() {
     return (
         <div className="form-block">
-            <div className="input-name-field">
+            <div className="input-field">
                 <p>Username</p>
                 <input className="input-username" placeholder="Type your username here..."/>
-            </div>
 
-            <br/>
 
-            <div className="input-password-field">
+                <br/>
+
                 <p>Password</p>
                 <input className="input-password" placeholder="Type your password here..."/>
             </div>
-
-            <p>Doesn't have an account? </p>
-            <Link to="/register" className="sign-up-link">Sign up here!</Link>
+            <div className="ext-field">
+                <p>Doesn't have an account?</p>
+                <Link to="/register" className="sign-up-link"> Sign up here!</Link>
+            </div>
         </div>
     );
 }
