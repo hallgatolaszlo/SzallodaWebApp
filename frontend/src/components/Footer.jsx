17e5e80d540c7e39870a5f5d4695d7c007926a65
Susmaher <troltom.122@gmail.com>
2025-05-21 11:50:08 +0200

    css bug fixes (sort of, temporary changes at most)

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import "../css/Footer.css";
+
+function Footer() {
+    return (
+        <>
+            <footer>
+                <hr className="footer-hr"/>
+                <h3 className="footer-text footer-text-1">Hotel Atlantis || Premium Hotel</h3>
+                <div className="footer-contact">
+                    <h3 className="footer-text footer-text-2">Address: 31°15′15.53″N, 24°15′30.53″</h3>
+                    <h3 className="footer-text footer-text-2">Phone: +100 300 351 7614</h3>
+                    <h3 className="footer-text footer-text-2">E-mail: info@atlantishotel.com</h3>
+                </div>
+            </footer>
+        </>
+    );
+}
+
+
+export default Footer;
