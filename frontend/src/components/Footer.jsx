f069764f7adf82f3c4d02173a19044b994969c94
Susmaher <troltom.122@gmail.com>
2025-05-21 14:21:33 +0200

    footer done

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,9 +5,10 @@ function Footer() {
         <>
             <footer>
                 <hr className="footer-hr"/>
-                <h3 className="footer-text footer-text-1">Hotel Atlantis || Premium Hotel</h3>
+                <h3 className="footer-text footer-text-1">Hotel Atlantis <span
+                    className="line-icon">||</span> Premium Hotel</h3>
                 <div className="footer-contact">
-                    <h3 className="footer-text footer-text-2">Address: 31°15′15.53″N, 24°15′30.53″</h3>
+                    <h3 className="footer-text footer-text-2">Address: 31°15′15.53″N, 24°15′30.53″W</h3>
                     <h3 className="footer-text footer-text-2">Phone: +100 300 351 7614</h3>
                     <h3 className="footer-text footer-text-2">E-mail: info@atlantishotel.com</h3>
                 </div>
