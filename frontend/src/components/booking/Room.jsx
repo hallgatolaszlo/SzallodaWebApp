a9bb199d0d220f20c2247d557d9b54191b6fdc0b
hallgato.laszlo <hallgato.laszlo1124@gmail.com>
2025-05-22 14:31:00 +0200

    Improved booking page, added npm package, changed db

--- frontend/src/components/booking/Room.jsx
+++ frontend/src/components/booking/Room.jsx
@@ -1,7 +1,17 @@
 import "../../css/booking/Room.css";
+import React, {useState} from "react";
 
-function Room({room, startDate, endDate}) {
+function Room({room}) {
+    const priceChangePerPerson = 1000;
     const {name, price, capacity, image} = room;
+    const [numberOfGuests, setNumberOfGuests] = useState(1);
+    const fullPrice = price + (numberOfGuests - 1) * priceChangePerPerson;
+
+    let options = [];
+    for (let i = 1; i <= capacity; i++) {
+        options.push(i.toString());
+    }
+
     return (
         <div className="room-container">
             <div className="room-title-bar">
@@ -10,10 +20,17 @@ function Room({room, startDate, endDate}) {
             </div>
             <div className="room-image-container">
                 <img className="room-image" src={image} alt={"Picture of " + name}/>
+                <p className="room-price">{"$" + new Intl.NumberFormat("US-us").format(fullPrice)}</p>
             </div>
             <div className="room-main">
-                <p className="room-date">{"Date: " + startDate.toLocaleDateString() + " - " + endDate.toLocaleDateString()}</p>
-                <p className="room-price">{"Price: €" + new Intl.NumberFormat("EU-eu").format(price)}</p>
+                <div className="room-number-of-guests-container">
+                    <p>{"Number of Guests:"}</p>
+                    {options.length > 1 ? <select onChange={(e) => {
+                        setNumberOfGuests(Number(e.target.value));
+                    }} className="room-number-of-guests-select">
+                        {options.map(option => React.createElement("option", {key: option[0]}, option))}
+                    </select> : React.createElement("p", {}, "1")}
+                </div>
             </div>
         </div>
     );
