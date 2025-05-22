6408aaac35c45b4869d475f5271a07f97ca3fce2
hallgatolaszlo <hallgato.laszlo1124@gmail.com>
2025-05-22 09:35:45 +0200

    Delete frontend/src/services/ratingAPI.js

--- frontend/src/services/ratingAPI.js
+++
@@ -1,9 +0,0 @@
-//LACINAK: EZ majd átkerülhetne a rendes api.js-be, de abba nem mertem plusz dolgot írni
-
-const BASE_URL = "http://localhost:3000/";
-
-
-export async function getRatingsFromAPI() {
-    const response = await fetch(`${BASE_URL}ratings`);
-    return response.json();
-}
