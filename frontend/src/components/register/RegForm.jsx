4bb66c5ffb1c03c239d50b849649baddb4bbffb6
rab.mihaly.jozsef <rabmisi1010@gmail.com>
2025-05-22 14:30:15 +0200

    register css v1.0 done

--- frontend/src/components/register/RegForm.jsx
+++ frontend/src/components/register/RegForm.jsx
@@ -21,27 +21,28 @@ function RegForm() {
     }, []);
 
     return (
-        <div className="login-register-container">
-            <div className="reg-form-inputs">
+        <div className="form-block">
+
+            <div className="input-field">
 
                 <div className="reg-form-block-header">
                     <p>Username</p>
-                    <input placeholder="New username..."></input>
+                    <input placeholder="New username..." className="input-username"></input>
                     <p>Password</p>
-                    <input type="password" placeholder="Password..."></input>
+                    <input type="password" placeholder="Password..." className="input-password"></input>
                     <p>Confirm password</p>
-                    <input type="password" placeholder="Confirm password..."></input>
+                    <input type="password" placeholder="Confirm password..." className="input-password-confirm"></input>
                 </div>
 
                 <div className="reg-form-block-buttons">
-                    <button>Sign up</button>
+                    <button className="register-button">Sign up</button>
                     <label>OR</label>
-                    <button>Log in</button>
+                    <button className="login-button">Log in</button>
                 </div>
 
             </div>
         </div>
-    );
+    )
 }
 
 export default RegForm;
