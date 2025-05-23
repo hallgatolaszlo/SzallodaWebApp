e7b59658139703fab40f9e7c13d42304a3a32b22
rab.mihaly.jozsef <rabmisi1010@gmail.com>
2025-05-23 13:03:49 +0200

    added room and lobbies gallery pages, added routs to them and started the css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import Booking from "./pages/Booking.jsx";
 import Admin from "./pages/Admin.jsx";
 import Register from "./pages/Register.jsx";
 import Gallery from "./pages/Gallery.jsx";
+import GalleryRooms from "./pages/GalleryRooms.jsx";
+import GalleryLobbies from "./pages/GalleryLobbies.jsx";
 import {Route, Routes} from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import {RoomsAndBookingsContextProvider} from "./contexts/RoomsAndBookingsContext.jsx";
@@ -23,6 +25,8 @@ function App() {
                         <Route path="/admin" element={<Admin/>}></Route>
                         <Route path="/register" element={<Register/>}></Route>
                         <Route path="/gallery" element={<Gallery/>}></Route>
+                        <Route path="/gallery-lobbies" element={<GalleryRooms/>}></Route>
+                        <Route path="/gallery-rooms" element={<GalleryLobbies/>}></Route>
                     </Routes>
                 </main>
             </RoomsAndBookingsContextProvider>
--- frontend/src/components/gallery/Images.jsx
+++ frontend/src/components/gallery/Images.jsx
@@ -1,15 +1,17 @@
-import {imageData} from "../../components/imageData.jsx";
 import '../../css/gallery/Images.css'
+import {useNavigate} from 'react-router-dom';
 
 function Images() {
-
+    const navigate = useNavigate();
 
     return (
         <div className="gallery-page-buttons-container">
-            <div className="gallery-page-inside-button">
+            <div className="gallery-page-inside-button"
+                 onClick={() => navigate("/gallery-rooms")}>
                 Rooms
             </div>
-            <div className="gallery-page-bar-button">
+            <div className="gallery-page-bar-button"
+                 onClick={() => navigate("/gallery-lobbies")}>
                 Lobby
             </div>
         </div>
