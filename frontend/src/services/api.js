62c1ebcd8d3f80631f15fe3b36d6311ba1bb8df1
Laszlo Hallgato <hallgato.laszlo1124@gmail.com>
2025-05-28 22:38:23 +0200

    added empty public folder for remote server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:3000/";
+// const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "https://szallodawebapp.onrender.com/";
 
 export async function getFromAPI(endpoint) {
     const response = await fetch(`${BASE_URL}${endpoint}`);
