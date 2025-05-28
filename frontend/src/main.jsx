08e19d6a6f39826a98bdfec1b10f971f6f298416
Laszlo Hallgato <hallgato.laszlo1124@gmail.com>
2025-05-28 22:04:42 +0200

    router fix (again)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter basename={import.meta.env.PROD ? '/SzallasWebApp' : '/'}>
+        <BrowserRouter basename={import.meta.env.PROD ? '/SzallodaWebApp' : '/'}>
             <App/>
         </BrowserRouter>
     </StrictMode>
