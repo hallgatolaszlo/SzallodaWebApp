af4cbb214b6e90c21bd574f0dbaf23ca0a803f88
Laszlo Hallgato <hallgato.laszlo1124@gmail.com>
2025-05-31 08:18:06 +0200

    finished redesigning login/register pages

--- frontend/src/components/register/RegForm.jsx
+++ frontend/src/components/register/RegForm.jsx
@@ -1,7 +1,7 @@
-import '../../css/register/RegForm.css'
+import '../../css/login/Form.css';
 import {getFromAPI, postAccounts} from "../../services/api.js";
 import {useEffect, useState} from "react";
-import {useNavigate} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 
 function RegForm() {
@@ -36,57 +36,49 @@ function RegForm() {
                         navigate("/login", {replace: true});
                     })
                     .catch(error => {
-                        console.log(error)
-                    })
+                        console.log(error);
+                    });
 
             }
         }
     }
 
-    function returnToLoginPage() {
-        navigate("/login", {replace: true});
-    }
-
-
     return (
         <div className="form-block">
-
-            <div className="input-field">
-
-                <div className="reg-form-block-header">
+            <div className="input-area">
+                <div className="input-field">
                     <p>Username</p>
                     <input placeholder="New username..."
-                           className="input-username-reg"
+                           className="input-username"
                            value={username}
                            onChange={(e) => setUsername(e.target.value)}></input>
+                </div>
+                <div className="input-field">
                     <p>Password</p>
                     <input type="password"
                            placeholder="Password..."
-                           className="input-password-reg"
+                           className="input-password"
                            value={password}
                            onChange={(e) => setPassword(e.target.value)}><
                     /input>
+                </div>
+                <div className="input-field">
                     <p>Confirm password</p>
                     <input type="password"
                            placeholder="Confirm password..."
-                           className="input-password-confirm"
+                           className="input-password"
                            value={cpassword}
                            onChange={(e) => setCpassword(e.target.value)}/>
                 </div>
-
-                <div className="reg-form-block-buttons">
-                    <button className="register-button" onClick={tryAddAccount}>Sign up</button>
-                    <button className="login-button-r" onClick={returnToLoginPage}>Log in</button>
-                </div>
-
+                <button className="login-button" onClick={tryAddAccount}>Sign up</button>
+            </div>
+            <div className="text-field">
+                <p>Already have an account?</p>
+                <Link to="/login" className="sign-up-link"> Log in</Link>
             </div>
 
-            <div className="logi-button-field-r">
-
-
-            </div>
         </div>
-    )
+    );
 }
 
 export default RegForm;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,14 +1,12 @@
-import "../css/register/Register.css"
+import "../css/register/Register.css";
 import RegForm from "../components/register/RegForm.jsx";
 
 function Register() {
     return (
-        <div className='register-page-container'>
-            <div className="form-container-finale">
-                <RegForm/>
-            </div>
+        <div className="register-page-container">
+            <RegForm/>
         </div>
-    )
+    );
 }
 
 export default Register;
